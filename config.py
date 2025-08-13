9f8ed18696cd444f4374bd68b47951fa1234b9c4
buck-lobo <ronnie_buck@hotmail.com>
2025-08-13 09:53:55 -0300

    preparando ambiente para inclusão da versão com firebase

--- config.py
+++ config.py
@@ -9,12 +9,18 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 ADMIN_ID = int(os.environ.get("ADMIN_ID")) if os.environ.get("ADMIN_ID") else None
 
-# Configurações do Firebase
+# Configurações do Firebase (variáveis de ambiente individuais)
+FIREBASE_TYPE = os.environ.get("FIREBASE_TYPE")
 FIREBASE_PROJECT_ID = os.environ.get("FIREBASE_PROJECT_ID")
-FIREBASE_CREDENTIALS_PATH = os.environ.get("FIREBASE_CREDENTIALS_PATH")
-
-# Para usar credenciais via JSON string (recomendado para deploy)
-FIREBASE_CREDENTIALS_JSON = os.environ.get("FIREBASE_CREDENTIALS_JSON")
+FIREBASE_PRIVATE_KEY_ID = os.environ.get("FIREBASE_PRIVATE_KEY_ID")
+FIREBASE_PRIVATE_KEY = os.environ.get("FIREBASE_PRIVATE_KEY")
+FIREBASE_CLIENT_EMAIL = os.environ.get("FIREBASE_CLIENT_EMAIL")
+FIREBASE_CLIENT_ID = os.environ.get("FIREBASE_CLIENT_ID")
+FIREBASE_AUTH_URI = os.environ.get("FIREBASE_AUTH_URI")
+FIREBASE_TOKEN_URI = os.environ.get("FIREBASE_TOKEN_URI")
+FIREBASE_AUTH_PROVIDER_X509_CERT_URL = os.environ.get("FIREBASE_AUTH_PROVIDER_X509_CERT_URL")
+FIREBASE_CLIENT_X509_CERT_URL = os.environ.get("FIREBASE_CLIENT_X509_CERT_URL")
+FIREBASE_UNIVERSE_DOMAIN = os.environ.get("FIREBASE_UNIVERSE_DOMAIN")
 
 # Configurações das coleções no Firestore
 COLLECTION_USUARIOS = "usuarios"
